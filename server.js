913701e3cbf76c3a522c354c72cfc895fb0e3014
DKMitt <dkmitt@sbcglobal.net>
2017-06-23 19:54:20 -0500

    routing

--- server.js
+++ server.js
@@ -40,6 +40,14 @@ db.once("open", function() {
 
 
 // ROUTES
+
+// Main "/" Route. This will redirect the user to our rendered React app
+// line 47 might need /public removed - have to see how it works
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
 // Get - This is the route components will use this to query MongoDB for all saved articles.
 // app.get("/api/saved", function(req, res) {
 
@@ -81,87 +89,6 @@ db.once("open", function() {
   
 
 // });
-
-
-// // Main "/" Route. This will redirect the user to our rendered React application
-// app.get("/", function(req, res) {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
-
-
-// // EXAMPLE ACTIVITY 18.4 20 SCRAPING
-// // A GET request to scrape the echojs website
-// app.get("/scrape", function(req, res) {
-//   // First, we grab the body of the html with request
-//   request("http://www.nytimes.com/", function(error, response, html) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(html);
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $("article h2").each(function(i, element) {
-
-//       // Save an empty result object
-//       var result = {};
-
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(this).children("a").text();
-//       result.link = $(this).children("a").attr("href");
-
-//       // Using our Article model, create a new entry
-//       // This effectively passes the result object to the entry (and the title and link)
-//       var entry = new Article(result);
-
-//       // Now, save that entry to the db
-//       entry.save(function(err, doc) {
-//         // Log any errors
-//         if (err) {
-//           console.log(err);
-//         }
-//         // Or log the doc
-//         else {
-//           console.log(doc);
-//         }
-//       });
-
-//     });
-//   });
-//   // Tell the browser that we finished scraping the text
-//   res.send("Scrape Complete");
-// });
-
-// // This will get the articles we scraped from the mongoDB
-// app.get("/articles", function(req, res) {
-//   // Grab every doc in the Articles array
-//   Article.find({}, function(error, doc) {
-//     // Log any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Or send the doc to the browser as a json object
-//     else {
-//       res.json(doc);
-//     }
-//   });
-// });
-
-// // Grab an article by it's ObjectId
-// app.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   Article.findOne({ "_id": req.params.id })
-//   // ..and populate all of the notes associated with it
-//   .populate("note")
-//   // now, execute our query
-//   .exec(function(error, doc) {
-//     // Log any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the doc to the browser as a json object
-//     else {
-//       res.json(doc);
-//     }
-//   });
-// });
-
 
 
 
